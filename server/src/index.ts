9faf7d7e7f8f0ad823e3ed119fbfd4307d0e88f4
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2022-03-26 00:31:07 +0300

    Refactor : use @swc-node with nodemon

--- server/src/index.ts
+++ server/src/index.ts
@@ -11,7 +11,7 @@ async function start() {
   server.applyMiddleware({ app })
   // eslint-disable-next-line no-promise-executor-return
   await new Promise<void>(resolve => httpServer.listen({ port: 8080 }, resolve))
-  // TODO: don't forget to remove before send in prod
+  // TODO: don't forget to remove 'console.log' before send in prod
   // eslint-disable-next-line no-console
   console.log(`🚀 Server ready at http://localhost:8080
     -for REST at /api
