41f6659b1f4aa07502c77051fc69c449ca8faf94
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2022-04-10 15:50:17 +0300

    Style | Refactor : restyle commande with tailwindcss

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,15 @@
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        base: '0 2px 4px rgba(10, 30, 30, 0.3)',
+        hover: '2px 4px 16px rgba(10, 30, 30, 0.6)',
+      },
+    },
     fontFamily: {
       sans: ['Poppins', 'sans-serif'],
-    }
+    },
   },
   plugins: [],
 }
